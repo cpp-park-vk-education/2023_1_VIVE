c0d4a5c86c3d82df1d0133018e5aef2cc0ca686d
Vladimir Konoplyuk <konoplyuk.v@yandex.ru>
2023-06-01 23:30:47 +0300

    minor changes

--- game/objects/Trigger/trigger.cpp
+++ game/objects/Trigger/trigger.cpp
@@ -7,9 +7,9 @@ void Trigger::initSprite()
     shape_.setFillColor(sf::Color::Blue);
     shape_.setSize(hitbox_.getSize());
     shape_.setPosition(hitbox_.getPosition());
-    std::cout << shape_.getPosition().x << ", " << shape_.getPosition().y
-              << "\t" << hitbox_.getPosition().x << ", "
-              << hitbox_.getPosition().y << std::endl;
+    // std::cout << shape_.getPosition().x << ", " << shape_.getPosition().y
+    //           << "\t" << hitbox_.getPosition().x << ", "
+    //           << hitbox_.getPosition().y << std::endl;
 }
 
 Trigger::Trigger(const sf::Vector2f size, const sf::Vector2f position)
